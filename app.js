2d43757ba89ed72538489110398fe4056e0b919d
Login <mail@gmail.com>
2026-08-25 09:15:05 +0400

    added new file

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 //firstName = "Emma"; 
 //console.log('Hello', firstName);
 
-let firstName = "Sophia";
-firstName = "Emma";
-console.log('Hello', firstName);
+let firstName = "Emma";
+firstName = "Sophia";
+console.log('Hello, my name is', firstName);
+let age = 24;
+console.log('I am', age, 'years old.');
+let city = "Tbilisi";
+console.log('I live in', city);
+let hobbies = "dancing and djing";
+console.log('My hobbies are', hobbies);
